5365790be8c6682c39fcbf95be2f566efe6758fd
hossainseu <32569710+hossainseu@users.noreply.github.com>
2021-03-27 23:06:53 +0600

    component done
    component done

--- website/resources/views/Home.blade.php
+++ website/resources/views/Home.blade.php
@@ -1,1 +1,12 @@
 @extends('layout.app')
+@section('content')
+
+@include('component.HomeBanner')
+@include('component.HomeService')
+@include('component.HomeCourse')
+@include('component.HomeProject')
+@include('component.HomeContact')
+@include('component.HomeBlog')
+@include('component.HomeReview')
+
+@endsection
